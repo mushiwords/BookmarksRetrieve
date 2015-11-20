f873e079578ac6054c251eba37d1dca0229091a0
yaung <gaoyang615@163.com>
2015-11-20 18:17:09 +0800

    Update funtion.js

--- jscript/funtion.js
+++ jscript/funtion.js
@@ -1,43 +1,59 @@
-window.onload=function(){
+$(function(){
+    var lowerCase = new Array();
+    var $content = $("#content_id");
+    $.getJSON("data/bookmarks.json",function(data){
+        var strHtml = "";
+        $$content.empty();
+        $.each(data,function(infoIndex,info){
+            var myDate = new Date( info["created"] *1000);
+            var realtime = myDate.toLocaleString();
+            strHtml +="<div>";
+            strHtml += "<div class=\'title\'>"+info["title"]+"</div>";
+            strHtml += "<div class=\'time\'>"+"Created @ "+realtime+"</div>";
+            strHtml +="</div>";
+            strHtml += "<hr class=\'hr\'>"
+            info = _.mapObject(info, function(val, key) {
+                return val.toLowerCase();
+            });
+            lowerCase.push(info);
+          })
+        $content.html(strHtml);
+      });
 
-	loadData();
-	$(".search")
-		.bind('keyup',function(event) {
-			$(this).css({
-					color: '#4499ee'
-				});
-			$(".list_content").map(function(index, val) {
-				var temp = this.innerHTML.replace(/<.*?>/ig,"");	
-				this.innerHTML = temp;
-			});
-			var keywords = this.value;
-			if(this.value !=""){
-				var keywordRE = new RegExp("(" + keywords + ")", "ig");
-				$(".list_content").each(function(){	
-					var text = this.innerHTML.toLocaleString();				
-					if(text.match(keywordRE)){
-						console.log(text);
-						var highlightedText = text.replace(keywordRE, "<span class='highlight'>$1</span>");							
-						this.innerHTML = highlightedText;
-						var $li = $(this).parent("li").show();
-					}
-					else{
-						var $li = $(this).parent("li").hide();	
-					}
-				});
-			}
-			else{
-				$("li").show();
-			}
-		});
-}
+    $("#textbox").keyup(function(){
+        var value = $(this).val();
+        $content.empty();
+        // var test = _.filter(lowerCase, function(object,infoIndex){
+        //   return object.title.indexOf(value); });
+        $.getJSON("data/bookmarks.json",function(data){
+            var strHtml = "";
+            $.each(lowerCase,function(infoIndex,info){
+                if (info["title"].indexOf(value.toLowerCase()) !=-1) {
+                    var myDate = new Date( info["created"] *1000);
+                    var realtime = myDate.toLocaleString();
+                    var position = info["title"].indexOf(value.toLowerCase());
+                    var defaultTitle = data[infoIndex].title;
+                    var title =  defaultTitle.substring(0,position) + "<mark>";
+                    title +=defaultTitle.substring(position,position+value.length) + "</mark>";
+                    title += defaultTitle.substring(position+value.length,data[infoIndex].length);
+                    strHtml +="<div>";
+                    strHtml += "<div class=\'title\'>"+title+"</div>";
+                    strHtml += "<div class=\'time\'>"+"Created @ "+realtime+"</div>";
+                    strHtml +="</div>";
+                    strHtml += "<hr class=\'hr\'>"
+                }
+              })
+              $content.html(strHtml);
+      });
+      });
+})
 
 function loadData(){
 	$('ul').children('li').remove();
 	$.getJSON("./data/bookmarks.json",function(content,status){
 		for(var i in content){
-			$(".list").append("<li><div class='list_content'>"
-				+ content[i]["title"]+"</div><div class='list_time'>@created &nbsp"
+			$(".list").append("<li><div>"
+				+ content[i]["title"]+"</div><div>@created &nbsp"
 				+ formatDate(content[i]["created"])+"</div></li>");
 		}
 		alert("load success");
